df6bc3511fb0467cb2f4f3f229f611e2213520cd
Murf-y <66155122+Murf-y@users.noreply.github.com>
2021-02-17 21:41:03 +0200

    f

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -269,7 +269,7 @@ class Moderation(commands.Cog):
     @commands.command(name="slowmode", description="وضع slowmode على قناة و يحدد الوقت بل صيغة التالية s/m/h, يمكن ازالته بوضع "
                                                    "القيمة الى صفر.\n\nيجب ان تكون من المشرفين لإستخدامها. ")
     @commands.has_role(const.moderator_role_name)
-    async def slowmode_async(self, ctx,, duration= TimeConverter):
+    async def slowmode_async(self, ctx, duration= TimeConverter):
         channel = ctx.channel
         if 0 <= duration <= 21600:
             await channel.edit(slowmode_delay=duration)
